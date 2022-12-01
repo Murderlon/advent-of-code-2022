392fd3087a1aa3a71ab4861d54c169e5f4f9b6f6
Murderlon <merlijn@soverin.net>
2022-12-01 18:30:41 -0300

    Simplify day 1

--- day-1/main.go
+++ day-1/main.go
@@ -9,36 +9,6 @@ import (
 )
 
 func main() {
-	fmt.Printf("Part one: %d\n", one())
-	fmt.Printf("Part two: %d\n", two())
-}
-
-func one() int {
-	file, _ := os.Open("day-1/input.txt")
-	scanner := bufio.NewScanner(file)
-	max := 0
-	current := 0
-
-	defer file.Close()
-
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		if len(line) == 0 {
-			if current > max {
-				max = current
-			}
-			current = 0
-		} else {
-			number, _ := strconv.Atoi(line)
-			current += number
-		}
-	}
-
-	return max
-}
-
-func two() int {
 	file, _ := os.Open("day-1/input.txt")
 	scanner := bufio.NewScanner(file)
 	slice := make([]int, 0)
@@ -58,7 +28,9 @@ func two() int {
 		}
 	}
 	slice = append(slice, current)
-	sort.Ints(slice)
-	top := slice[len(slice)-3:]
-	return top[0] + top[1] + top[2]
+	sort.Sort(sort.Reverse(sort.IntSlice(slice)))
+	top := slice[:3]
+
+	fmt.Printf("Part one: %d\n", top[0])
+	fmt.Printf("Part two: %d\n", top[0]+top[1]+top[2])
 }
