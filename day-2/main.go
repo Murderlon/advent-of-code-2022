79f1a759598cfd2e594e039730f2ec5307b2ee59
Murderlon <merlijn@soverin.net>
2022-12-03 10:37:11 -0300

    Simplify day 2

--- day-2/main.go
+++ day-2/main.go
@@ -8,22 +8,28 @@ import (
 )
 
 const (
-	rock    = 1
-	paper   = 2
-	scissor = 3
+	A = 1
+	B = 2
+	C = 3
 
 	win  = 6
 	draw = 3
 )
 
 var alias = map[string]string{
-	"X": "rock",
-	"A": "rock",
-	"Y": "paper",
-	"B": "paper",
-	"Z": "scissor",
-	"C": "scissor",
+	"X": "A",
+	"Y": "B",
+	"Z": "C",
 }
+
+var game = map[string]map[string]int{
+	"A": {"A": A + draw, "B": B + win, "C": C},
+	"B": {"A": A, "B": B + draw, "C": C + win},
+	"C": {"A": A + win, "B": B, "C": C + draw},
+}
+var mustWin = map[string]string{"A": "B", "B": "C", "C": "A"}
+var mustDraw = map[string]string{"A": "A", "B": "B", "C": "C"}
+var mustLoose = map[string]string{"A": "C", "B": "A", "C": "B"}
 
 func main() {
 	fmt.Printf("Part one: %d\n", one())
@@ -33,18 +39,13 @@ func main() {
 func one() int {
 	file, _ := os.Open("day-2/input.txt")
 	scanner := bufio.NewScanner(file)
-	game := map[string]map[string]int{
-		"rock":    {"rock": rock + draw, "paper": paper + win, "scissor": scissor},
-		"paper":   {"rock": rock, "paper": paper + draw, "scissor": scissor + win},
-		"scissor": {"rock": rock + win, "paper": paper, "scissor": scissor + draw},
-	}
 	total := 0
 
 	defer file.Close()
 
 	for scanner.Scan() {
 		picks := strings.Fields(scanner.Text())
-		them := alias[picks[0]]
+		them := picks[0]
 		me := alias[picks[1]]
 
 		total += game[them][me]
@@ -56,20 +57,17 @@ func one() int {
 func two() int {
 	file, _ := os.Open("day-2/input.txt")
 	scanner := bufio.NewScanner(file)
-	mustWin := map[string]int{"rock": paper + win, "paper": scissor + win, "scissor": rock + win}
-	mustDraw := map[string]int{"rock": rock + draw, "paper": paper + draw, "scissor": scissor + draw}
-	mustLoose := map[string]int{"rock": scissor, "paper": rock, "scissor": paper}
-  game := map[string]map[string]int{"Z": mustWin, "Y": mustDraw, "X": mustLoose}
+	instruction := map[string]map[string]string{"Z": mustWin, "Y": mustDraw, "X": mustLoose}
 	total := 0
 
 	defer file.Close()
 
 	for scanner.Scan() {
 		picks := strings.Fields(scanner.Text())
-		them := alias[picks[0]]
-		me := picks[1]
+		them := picks[0]
+		me := instruction[picks[1]][them]
 
-    total += game[me][them]
+		total += game[them][me]
 	}
 
 	return total
